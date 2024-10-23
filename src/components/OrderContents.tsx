bb81c95a1f477f89971b143c6b5dab6c9d47f41c
carlox43 <carlosekosalazar@gmail.com>
2024-10-23 16:57:15 -0300

    1.1

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,14 +1,13 @@
-import { MenuItems, OrderItem } from "../types/index";
+import { OrderItem } from "../types/index";
 import { formatCurrency } from "../helpers/index";
+import { Dispatch } from "react";
+import { OrderActions } from "../reducers/order-reducer";
 type OrderContentsProps = {
   order: OrderItem[];
-  removeItem: (id: MenuItems["id"]) => void;
+  dispatch: Dispatch<OrderActions>;
 };
 
-export default function OrderContents({
-  order,
-  removeItem,
-}: OrderContentsProps) {
+export default function OrderContents({ order, dispatch }: OrderContentsProps) {
   return (
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
@@ -29,7 +28,9 @@ export default function OrderContents({
             </div>
             <button
               className="bg-red-600 h-8 w-8 rounded-full text-white text-black"
-              onClick={() => removeItem(item.id)}
+              onClick={() =>
+                dispatch({ type: "remove-item", payload: { id: item.id } })
+              }
             >
               X
             </button>
